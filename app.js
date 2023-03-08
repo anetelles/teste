f4951b67896b2e0b57b8442284d626be99356f1e
Cristiane Telles <108342343+anetelles@users.noreply.github.com>
2023-03-08 15:34:26 -0300

    v.0.2

--- app.js
+++ app.js
@@ -14,76 +14,74 @@ var port = 3000
 
 //Configuração handlesbar:
 app.engine('handlebars', exphbs.engine())
-app.set('view engine','handlebars')
-
-
+app.set('view engine', 'handlebars')
 
 //Rota:
 //Rota raíz:
-app.get('/', function(req,res){
-    res.render('home', { layout: false})
+app.get('/', function (req, res) {
+  res.render('home', { layout: false })
 })
 
 //Express URL:
 app.use(
-    express.urlencoded({
-        extended: true
-        
-  }) 
-  )
-  //Rota para inserir dados:
-  app.post('/prod/insertprod', (req, res) => {
-    const nome = req.body.nome
-    const quantidade = req.body.quantidade
-  
-    const sql = `INSERT INTO produto (nome, quantidade) VALUES ('${nome}', '${quantidade}')`
-  
-    conn.query(sql, function(err){
-        if (err){
-            console.log(err)
-        }
-  
-        res.redirect('/')
-    })
-  })
+  express.urlencoded({
+    extended: true
 
-  
-  //Rota de consulta geral:
-  app.get('/prod', (req, res) => {
-    const sql = 'SELECT * FROM produto'
-    
-    conn.query(sql, function(err, data){
-      if(err){
-        console.log(err)
-        return
-      }
-      
-      const listar = data
-      
-      console.log(listar)
-      
-      res.render('prod', { layout: false, listar })
-      
-    })
   })
-  
-  //Consultar um registo pelo id (produto.handlebars):
-  app.get('/prod/:id', (req, res) => {
-    const id = req.params.id
-    
-    const sql = `SELECT * FROM produto WHERE id = ${id}`
-  
-    conn.query(sql, function(err, data){
-        if(err){
-            console.log(err)
-            return
-        }
-  
-        const listarProd = data[0]
-        res.render('produto', {  layout: false, listarProd } )
-  
-    })
+)
+
+//Rota para inserir dados:
+app.post('/prod/insertprod', (req, res) => {
+  const nome = req.body.nome
+  const quantidade = req.body.quantidade
+
+  const sql = `INSERT INTO produto (nome, quantidade) VALUES ('${nome}', '${quantidade}')`
+
+  conn.query(sql, function (err) {
+    if (err) {
+      console.log(err)
+    }
+
+    res.redirect('/')
   })
+})
+
+//Rota de consulta geral:
+app.get('/prod', (req, res) => {
+  const sql = 'SELECT * FROM produto'
+
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err)
+      return
+    }
+
+    const listar = data
+
+    console.log(listar)
+
+    res.render('prod', { layout: false, listar })
+
+  })
+})
+
+//Consultar um registo pelo id (produto.handlebars):
+app.get('/prod/:id', (req, res) => {
+  const id = req.params.id
+
+  const sql = `SELECT * FROM produto WHERE id = ${id}`
+
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err)
+      return
+    }
+
+    const listarProd = data[0]
+    res.render('produto', { layout: false, listarProd })
+
+  })
+})
 
 //Rota do buscar:
 app.get('/busca', (req, res) => {
@@ -95,31 +93,31 @@ app.post('/busc/', (req, res) => {
   const id = req.body.id
   const sql = `SELECT * FROM produto WHERE id = ${id}`
 
-  conn.query(sql, function(err, data){
-     if(err){
-     console.log(err)
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err)
       return
     }
-     const listarProd = data[0]
-     res.render('produto', {  layout: false, listarProd } )
-     })
-    })
+    const listarProd = data[0]
+    res.render('produto', { layout: false, listarProd })
+  })
+})
 
 //Pegando dado para editar registro:
 app.get('/prod/edit/:id', (req, res) => {
-    
+
   const id = req.params.id
 
   const sql = `SELECT * FROM produto where id = ${id}`
 
-  conn.query(sql, function(err, data){
-      if(err){
-          console.log(err)
-          return
-      }
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err)
+      return
+    }
 
-      const prod = data[0]
-      res.render('edit', { layout: false, prod } )
+    const prod = data[0]
+    res.render('edit', { layout: false, prod })
 
   })
 })
@@ -130,20 +128,19 @@ app.post('/prod/updateprod', (req, res) => {
   const id = req.body.id
   const nome = req.body.nome
   const quantidade = req.body.quantidade
-  
-  const sql = `UPDATE produto SET nome = '${nome}', quantidade = '${quantidade}' WHERE id = '${id}'` 
 
-  conn.query(sql, function(err) {
-      if(err){
-          console.log(err)
-          return
-      }
+  const sql = `UPDATE produto SET nome = '${nome}', quantidade = '${quantidade}' WHERE id = '${id}'`
 
-      res.redirect('/prod')
-  }) 
+  conn.query(sql, function (err) {
+    if (err) {
+      console.log(err)
+      return
+    }
+
+    res.redirect('/prod')
+  })
 
 })
-
 
 //Rota para deletar um registro:
 app.get('/prod/remove/:id', (req, res) => {
@@ -151,37 +148,36 @@ app.get('/prod/remove/:id', (req, res) => {
 
   const sql = `DELETE FROM produto WHERE id = '${id}'`
 
-  conn.query(sql, function(err){
-      if(err){
-          console.log(err)
-          return
-      }
+  conn.query(sql, function (err) {
+    if (err) {
+      console.log(err)
+      return
+    }
 
-      res.redirect('/prod')
+    res.redirect('/prod')
   })
 })
 
-
 //Conexão com o banco de dados:
 const conn = mysql.createConnection({
-    host: 'localhost',    
-    port: '3306',
-    user:'root',
-    password: '',
-    database: 'crud'
-  
-  })
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: '',
+  database: 'crud'
 
-  conn.connect(function(err) {
-    if(err){
-        console.log(err)
-    }
-  
-    console.log('Conectado com sucesso!')
-    
-  })
+})
+
+conn.connect(function (err) {
+  if (err) {
+    console.log(err)
+  }
+
+  console.log('Conectado com sucesso!')
+
+})
 
 //Configurar o servidor:
 app.listen(port, () => {
-    console.log(`App rodando na porta ${port}`)
+  console.log(`App rodando na porta ${port}`)
 })
